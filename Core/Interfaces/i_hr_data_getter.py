10c77874371434f3bc56dab67de4e29a33ce3009
theAppleist <sapirkraus@me.com>
2017-06-19 18:18:15 +0300

    Finished JsonHRDB Mock

---
+++ Core/Interfaces/i_hr_data_getter.py
@@ -0,0 +1,3 @@
+class IHRDataGetter(object):
+    def get_hr_data_by_id(self, id_number):
+        raise NotImplementedError
--- DAL/HRDBCommunicator/json_hr_db_communicator.py
+++ DAL/HRDBCommunicator/json_hr_db_communicator.py
@@ -1,14 +1,23 @@
 import json
 
-# Mock Data Path
-MOCK_HR_DATA_DB_PATH="mock_elaborate.json"
+from Core.Keys.hr_data_keys import *
+from Core.Interfaces.i_hr_data_getter import IHRDataGetter
 
-class JsonHrDbCommunicator(object):
+# Mock Data Path
+MOCK_HR_DATA_DB_PATH = "mock_elaborate.json"
+
+
+class JsonHrDbCommunicator(IHRDataGetter):
     def __init__(self):
         self._data = self._get_data_mock(MOCK_HR_DATA_DB_PATH)
 
-    def get_hr_data_by_key(self, id, key):
-        pass
+    def get_hr_data_by_id(self, id_number):
+        if id_number in self._data:
+            try:
+                return json.dumps(self._data[id_number])
+            except Exception as e:
+                pass
+        return None
 
     @staticmethod
     def _get_data_mock(path):
@@ -19,5 +28,7 @@ class JsonHrDbCommunicator(object):
             except Exception as e:
                 print e
                 return None
+
         for person in data:
-            pass
+            hr_data[person[ID_NUMBER]] = person
+        return hr_data
