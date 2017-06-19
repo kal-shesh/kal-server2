296583b2555de54716b717c2b687931b9da32458
theAppleist <sapirkraus@me.com>
2017-06-20 01:04:54 +0300

    Added creation from dictionary methods

--- Core/Models/Form.py
+++ Core/Models/Form.py
@@ -1,7 +1,24 @@
-
-
 class Form:
     def __init__(self, uuid, form_data, form_metadata):
         self.uuid = uuid
         self.form_data = form_data
         self.form_metadata = form_metadata
+
+    @staticmethod
+    def create_from_dictionary(data):
+        from form_metadata import FormMetadata
+        from step import Step
+
+        steps = []
+        for s in data['form_metadata']['steps']:
+            steps.append(Step.create_step_from_dictionary(s))
+
+        metadata_dict = data['form_metadata']
+        metadata = FormMetadata(steps,
+                                metadata_dict['creation_time'],
+                                metadata_dict['last_update_time'],
+                                metadata_dict['form_type'],
+                                metadata_dict['creator_id'],
+                                metadata_dict['display_name'])
+        form = Form(data['uuid'], data['form_data'], metadata)
+        return form
--- Core/Models/step.py
+++ Core/Models/step.py
@@ -1,6 +1,19 @@
 class Step:
-    def __init__(self, description, status, approver_id, steps=[]):
+    def __init__(self, description, status, approver_id, steps=list()):
         self.description = description
         self.status = status
         self.approver_id = approver_id
         self.steps = steps
+
+    @staticmethod
+    def create_step_from_dictionary(data):
+        desc = data['description']
+        status = data['status']
+        approver_id = data['approver_id']
+        steps = []
+
+        for s in data['steps']:
+            steps.append(Step.create_step_from_dictionary(s))
+
+        step = Step(desc, status, approver_id, steps)
+        return step
