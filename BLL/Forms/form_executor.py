cdcc5ad55cd7d8805f206b7fda7c4158e3644ceb
theAppleist <sapirkraus@me.com>
2017-06-20 11:37:14 +0300

    fixes

--- BLL/Forms/form_executor.py
+++ BLL/Forms/form_executor.py
@@ -25,7 +25,7 @@ class FormExecutor(IFormExecutor):
         form_object = Form.create_from_dictionary(form_data)
         self.__generate_step_approvers(user_id, form_object.metadata.next_steps)
         self.__save_new_form(form_object)
-        #PdfGenerator.create_pdf(form_object)
+        PdfGenerator.create_pdf(form_object)
         return True
 
     def update_step(self, user_id, form_id, data):
@@ -33,7 +33,7 @@ class FormExecutor(IFormExecutor):
         if data['status'] in [APPROVED, REJECTED]:
             self.form_commincator.update_form(form)
             self.user_commincator.update_user_awaiting_forms_file(user_id, {REMOVED_KEY: [form_id]})
-            #PdfGenerator.create_pdf(form)
+            PdfGenerator.create_pdf(form)
         elif data['status'] in [WAITING]:
             for approver in self.__get_all_awaiting_approvers(form.form_metadata.next_steps):
                 self.user_commincator.update_user_awaiting_forms_file(approver, {ADDED_KEY: [form_id]})
